433e45d51462475a41c3cd2926e9fcacbb3d60f4
Stepan <soulsteff@gmail.com>
2024-08-07 10:47:31 +0300

    ver1.1

---
+++ src/main/java/Pair.java
@@ -0,0 +1,19 @@
+public class Pair {
+
+    double first;
+    double second;
+
+    public Pair (Double first, Double second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "Answer{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
+
+}
